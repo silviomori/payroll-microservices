35932c4613f440df1edf5d72a344c4eb3e37795d
Silvio Mori Neto <silviomori@gmail.com>
2021-03-20 11:32:11 -0300

    refactor: Encapsulate Worker RestContr. responses

--- hr-worker/src/main/java/com/technomori/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/technomori/hrworker/resources/WorkerResource.java
@@ -3,6 +3,7 @@ package com.technomori.hrworker.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +20,15 @@ public class WorkerResource {
 	private WorkerService workerService;
 	
 	@GetMapping
-	public List<Worker> findAll() {
-		return workerService.findAll();
+	public ResponseEntity<List<Worker>> findAll() {
+		List<Worker> workers = workerService.findAll();
+		return ResponseEntity.ok(workers);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public Worker findById(@PathVariable Long id) {
-		return workerService.findById(id);
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		Worker worker = workerService.findById(id);
+		return ResponseEntity.ok(worker);
 	}
+
 }
